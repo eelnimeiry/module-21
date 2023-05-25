3c30569c7e3d6515821f5645e73e19ac7d9fc23f
eelnimeiry <Elsadekelnimeriy@gmail.com>
2023-05-25 12:19:04 -0700

    make it better

--- server/config/DatabaseConnection.js
+++ server/config/DatabaseConnection.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://Muhammad_Akbar:Akbar.1234@cluster0.63uyc.mongodb.net/testingtest?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
